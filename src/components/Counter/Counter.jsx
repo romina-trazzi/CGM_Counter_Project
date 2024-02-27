cb7320454b803f9143e9bbe22272896d862686cb
Romina Trazzi <romina.trazzi@posta.it>
2024-02-27 15:05:39 +0100

    Progetto con memo, useMemo, useCallback

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,94 @@
+import { useState, memo, useCallback, useMemo } from 'react';
+
+import IconButton from '../UI/IconButton.jsx';
+import MinusIcon from '../UI/Icons/MinusIcon.jsx';
+import PlusIcon from '../UI/Icons/PlusIcon.jsx';
+import CounterHistory from './CounterHistory.jsx';
+import CounterOutput from './CounterOutput.jsx';
+import { log } from '../../log.js';
+
+function isPrime(number) {
+  log('Calculating if is prime number', 2, 'other');
+
+  if (number <= 1) {
+    return false;
+  }
+
+  const limit = Math.sqrt(number);
+
+  for (let i = 2; i <= limit; i++) {
+    if (number % i === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+const Counter = memo(function Counter({ initialCount }) {
+  log('<Counter /> rendered', 1);
+
+  const initialCountIsPrime = useMemo(
+    () => isPrime(initialCount),
+    [initialCount]
+  );
+
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([{value: initialCount, id: Math.random()*1000}]);
+
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange.value,
+    0
+  );
+
+  const handleDecrement = useCallback(function handleDecrement() {
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [{value: -1,  id: Math.random()*1000}, ...prevCounterChanges]);
+  }, []);
+
+  const handleIncrement = useCallback(function handleIncrement() {
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [{value: 1, id: Math.random()*1000}, ...prevCounterChanges]);
+  }, []);
+
+  return (
+    <section className="counter">
+      <p className="counter-info">
+        The initial counter value was <strong>{initialCount}</strong>. It{' '}
+        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+      </p>
+      <p>
+        <IconButton icon={MinusIcon} onClick={handleDecrement}>
+          Decrement
+        </IconButton>
+        <CounterOutput value={currentCounter} />
+        <IconButton icon={PlusIcon} onClick={handleIncrement}>
+          Increment
+        </IconButton>
+      </p>
+      <CounterHistory history={counterChanges}/>
+    </section>
+  );
+});
+
+export default Counter;
+
+
+/* Memo guarderà le props della funzione componente che sta wrappando.
+Nel caso in cui le props o i suoi stati interni cambiano, il componente viene ricaricato, 
+altrimenti no */
+
+/* Se i dati delle props sono oggetti o array bisogna per forza lavorare
+su copie degli oggetti originali con ... spread operator */
+
+/* Memo quindi impedisce l'esecuzione (ricoricamento) di componenti figli
+quando il componente padre viene eseguito.
+Questo impedisce inoltre il rieseguimento dei componenti annidati nel  
+componente figlio */
+
+/* Va usato il più in alto possibile nell'alberatura dei compoenti */
+
+
+/* useMemo invece serve a bloccare funzioni, l'array di dipendenze indica 
+quale valore deve cambiare per far rieseguire la funzione */
+
